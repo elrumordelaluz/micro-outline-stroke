e6a6b352b0b1b23658d6420e1b6e29ce0921dff8
Lionel T <elrumordelaluz@hotmail.com>
2017-12-03 00:39:58 +0100

    Add width/height params

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const outlineStroke = require('svg-outline-stroke')
 const parse = require('urlencoded-body-parser')
 
 module.exports = async (req, res) => {
-  const { input } = await parse(req)
+  const { input, width, height } = await parse(req)
   if (input) {
     try {
-      return await outlineStroke(input)
+      return await outlineStroke(input, { width, height })
     } catch (err) {
       send(res, 400, `Error: ${err}`)
     }
   } else {
-     send(res, 400, '"input" field is required')
+    send(res, 400, '"input" field is required')
   }
 
   return 'Data logged to your console'
