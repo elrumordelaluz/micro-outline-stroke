7af880c2326f3f2d25841995506e0e38884a040b
Lionel T <elrumordelaluz@hotmail.com>
2017-12-07 12:32:47 +0100

    Return responses

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const { send, json } = require('micro')
+const cors = require('micro-cors')()
 const outlineStroke = require('svg-outline-stroke')
-
 const parse = require('urlencoded-body-parser')
 
-module.exports = async (req, res) => {
+const handler = async (req, res) => {
   const { input, width, height } = await parse(req)
   if (input) {
     try {
       return await outlineStroke(input, { width, height })
     } catch (err) {
-      send(res, 400, `Error: ${err}`)
+      return send(res, 400, `Error: ${err}`)
     }
   } else {
-    send(res, 400, '"input" field is required')
+    return send(res, 400, '"input" field is required')
   }
-
-  return 'Data logged to your console'
 }
+
+module.exports = cors(handler)
