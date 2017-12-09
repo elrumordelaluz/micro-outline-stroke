63b938ae17132bea0993d349f60942c167a80010
Lionel T <elrumordelaluz@hotmail.com>
2017-12-09 01:27:55 +0100

    Remove unused params

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const outlineStroke = require('svg-outline-stroke')
 const parse = require('urlencoded-body-parser')
 
 const handler = async (req, res) => {
-  const { input, width, height } = await parse(req)
+  const { input } = await parse(req)
   if (input) {
+    console.log(input);
     try {
-      return await outlineStroke(input, { width, height })
+      return await outlineStroke(input)
     } catch (err) {
       return send(res, 400, `Error: ${err}`)
     }
